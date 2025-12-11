fc80ea9fda5e37cc741b933076c5bac8127b94dd
Nicolas0523 <bnur03@icloud.com>
2025-12-11 19:52:21 +0500

    Remove merge conflict markers from urls.py

--- erosion_project/erosion_project/urls.py
+++ erosion_project/erosion_project/urls.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -12,5 +12,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('analysis.urls')),
->>>>>>> 39381e6 (Update GEE code for Render)
 ]
