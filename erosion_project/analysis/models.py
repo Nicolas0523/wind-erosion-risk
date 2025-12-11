d9bec5907207597c3d923452d89af6a10fb54673
Nicolas0523 <bnur03@icloud.com>
2025-12-11 20:42:17 +0500

    Remove merge conflict markers from models.py

--- erosion_project/analysis/models.py
+++ erosion_project/analysis/models.py
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 from django.db import models
 
 # Create your models here.
-=======
+
 from django.db import models
 
 # Create your models here.
->>>>>>> 39381e6 (Update GEE code for Render)
