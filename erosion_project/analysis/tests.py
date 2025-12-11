7ee85c8cb6c8998b78ed1b92349f3b8787202c30
Nicolas0523 <bnur03@icloud.com>
2025-12-11 20:42:39 +0500

    Update tests.py

--- erosion_project/analysis/tests.py
+++ erosion_project/analysis/tests.py
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 from django.test import TestCase
 
 # Create your tests here.
-=======
+
 from django.test import TestCase
 
 # Create your tests here.
->>>>>>> 39381e6 (Update GEE code for Render)
