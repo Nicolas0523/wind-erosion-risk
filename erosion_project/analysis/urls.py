4f14c98e9c8e39c167f4390cee447bed3e39ac81
Nicolas0523 <bnur03@icloud.com>
2025-12-11 20:43:13 +0500

    Update urls.py

--- erosion_project/analysis/urls.py
+++ erosion_project/analysis/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.urls import path
 from . import views
 
@@ -11,5 +10,4 @@ urlpatterns = [
 urlpatterns = [
     path('', views.home, name='home'),
     path('get_data/', views.get_data, name='get_data'),
->>>>>>> 39381e6 (Update GEE code for Render)
 ]
