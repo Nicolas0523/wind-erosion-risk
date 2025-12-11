4b1678f80b10c0046d3cfc9e71f397bb9df3e86c
Nicolas0523 <bnur03@icloud.com>
2025-12-11 20:40:58 +0500

    Remove duplicate import and class definition

--- erosion_project/analysis/apps.py
+++ erosion_project/analysis/apps.py
@@ -1,15 +1,13 @@
-<<<<<<< HEAD
 from django.apps import AppConfig
 
 
 class AnalysisConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'analysis'
-=======
+
 from django.apps import AppConfig
 
 
 class AnalysisConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'analysis'
->>>>>>> 39381e6 (Update GEE code for Render)
